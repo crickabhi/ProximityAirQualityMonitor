83780c87077adf2c5e245765a10713e607e88ac0
Abhinav Mathur <crickabhi@gmail.com>
2021-06-22 17:08:33 +0530

    Initial Commit

---
+++ ProximityAirQualityMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProximityAirQualityMonitor
+//
+//  Created by Abhinav Mathur on 22/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
